b3b1315dad56a11f757bf9122581308b6522d359
YAMANE Toshiaki <yamanetoshi@gmail.com>
2024-02-14 08:16:00 +0000

    step 4

---
+++ lib/issues/cli.ex
@@ -0,0 +1,86 @@
+defmodule Issues.CLI do
+  @default_count 4
+
+  alias Issues.TableFormatter, as: TF
+
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various finctions that end up generating a
+  table of the last _n_ issues in a github project
+  """
+
+  def main(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def run(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  @doc """
+  `argv`can be -h or --help, which returns :help.
+
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
+
+  Return a tuple of`{user, project, count}`, or: `help` if help was given.
+  """
+
+  def parse_args(argv) do
+    OptionParser.parse(argv, switches: [help: :boolean],
+                             aliases:  [h: :help])
+      |> elem(1)
+      |> args_to_internal_representation()
+  end
+
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+
+  def args_to_internal_representation(_) do
+    :help
+  end
+
+  def process(:help) do
+  IO.puts """
+  usage: issues <user> <project> [ count | #{@default_count}]
+  """
+  System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+    |> TF.print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+          i1["created_at"] >= i2["created_at"]
+       end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse
+  end
+end
---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,32 @@
+defmodule Issues.GithubIssues do
+  require Logger
+
+  @user_agent [ {"User-agent", "Elixir dave@pragprog.com"}]
+
+  def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+#  @github_url Application.get_env(:issues, :github_url)
+  def issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+#    "#{@github_url}/repose/#{user}/#{project}/issues"
+  end
+
+  def handle_response({ _, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
+    {
+        status_code |> check_for_error(),
+        body |> Poison.Parser.parse!()
+    }
+  end
+
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_),   do: :error
+end
